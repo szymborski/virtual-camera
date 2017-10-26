0b1e4b252c89d4ff6d7e79a74bd482f95d4290de
grzch <fangitary@gmail.com>
2017-10-27 00:21:36 +0200

    Further refactor, added 'namespace' params

--- js/scene.js
+++ js/scene.js
@@ -1,5 +1,5 @@
 class Scene {
-    constructor(ctx, objects, canvasWidthMiddle, canvasHeightMiddle) {
+    constructor({ctx, objects, canvasWidthMiddle, canvasHeightMiddle}) {
         // ctx - canvas context
         // objects - objects to render
         this.objects = objects
@@ -14,32 +14,32 @@ class Scene {
 
     project(M) {
         // M -> face
-        // Distance between the camera and the plane
-        let r = this.distance / M.y;
+        // r -> distance between the camera and the plane
+        let r = this.distance / M.y
         // ignore points outside screen
-        if (M.y < 0) return undefined;
-        return new Vertex2D(r * M.x, r * M.z);
+        if (M.y < 0) return
+        return new Vertex2D(r * M.x, r * M.z)
     }
 
     render() {
         // clear the previous frame
-        this.ctx.clearRect(0, 0, 2 * this.canvasWidthMiddle, 2 * this.canvasHeightMiddle);
+        this.ctx.clearRect(0, 0, 2 * this.canvasWidthMiddle, 2 * this.canvasHeightMiddle)
 
         this.objects.forEach(object => {
             object.faces.forEach(face => {
-                this.ctx.beginPath();
+                this.ctx.beginPath()
                 face.forEach((vertex, i) => {
-                    let P = this.project(vertex);
+                    let P = this.project(vertex)
                     if (P === undefined) return
                     let x = P.x + this.canvasWidthMiddle
                     let y = -P.y + this.canvasHeightMiddle
                     // if first vertex - move to start position
                     if (i === 0) this.ctx.moveTo(x, y)
 
-                    this.ctx.lineTo(x, y);
+                    this.ctx.lineTo(x, y)
                 })
-                this.ctx.closePath();
-                this.ctx.stroke();
+                this.ctx.closePath()
+                this.ctx.stroke()
             })
         })
     }
