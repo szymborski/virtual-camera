26d8927bd3b5baf7de09c461b90c567409c88c87
grzch <fangitary@gmail.com>
2017-11-21 15:45:58 +0100

    Removed unnecessary function call

--- js/paintersAlgorithm.js
+++ js/paintersAlgorithm.js
@@ -1,5 +1,4 @@
 function getFaceAvg (face) {
-	calculateNormalVector(face);
     let sum = 0
     face.forEach(vertex => {
         sum += Math.sqrt(vertex.x ** 2 + vertex.y ** 2 + vertex.z ** 2)
@@ -25,13 +24,13 @@ function calculateNormalVector(face) {
 		vec1 = new Vertex(b.x - a.x, b.y - a.y, b.z - a.z),
 		vec2 = new Vertex(c.x - b.x, c.y - b.y, c.z - b.z),
 		normalVector;
-	
+
 	normalVector = {
 		x: vec1.y * vec2.z - vec1.z * vec2.y,
 		y: vec1.z * vec2.x - vec1.x * vec2.z,
 		z: vec1.x * vec2.y - vec1.y * vec2.x
 	}
-	
+
 	return normalVector;
 }
 
@@ -39,15 +38,15 @@ function eliminateInvisibleFaces(cube) {
  	let facesToShow = [],
 		i = 0,
 		facesLength = cube.faces.length;
- 
+
 	for (; i < facesLength; ++i) {
 		let face = cube.faces[i],
 			normalVector = calculateNormalVector(face),
 			facePoint = face[3];
-		
+
 		if((facePoint.x * normalVector.x + facePoint.y * normalVector.y + facePoint.z * normalVector.z) < 0 )
 			facesToShow.push(face);
 	}
- 	
+
  	return facesToShow;
  }
